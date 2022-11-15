25bf613c17b9ea7471611ac9e1c89ea249eb7c81
shinpei Kinjyou <e225712@eve.u-ryukyu.ac.jp>
2022-11-16 03:30:29 +0900

    trial version

---
+++ app/src/test/java/jp/ac/uryukyu/ie/e225712/EnemyTest.java
@@ -0,0 +1,25 @@
+package jp.ac.uryukyu.ie.e225712;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class EnemyTest {
+    @Test void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    
+    }
+    @Test
+void attackTest() {
+    int defaultHeroHp = 100;
+    Hero demoHero = new Hero("デモ勇者", defaultHeroHp, 100);
+    Enemy slime = new Enemy("スライムもどき", 10, 100);
+    for(int count=0; count<10; count++){
+        demoHero.attack(slime); // over kill
+    }
+    slime.attack(demoHero);
+    assertEquals(defaultHeroHp, demoHero.hitPoint);
+    }
+
+}
